d55a3a24652f68be5bb67ff3996c978edab1cca1
Stefan Geranliev <s.geranliev@abv.bg>
2022-03-31 22:08:48 +0300

    Update project

---
+++ petstagram/petstagram/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    FIRST_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MIN_LENGTH = 2
+    LAST_NAME_MAX_LENGTH = 30
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+    )
